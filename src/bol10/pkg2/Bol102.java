381c3f449c3e0ec49874dc11662265bb9e219a33
propa <propa@DESKTOP-10ADTBO>
2017-06-20 15:37:07 +0200

    Acabado

---
+++ src/bol10/pkg2/Bol102.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol10.pkg2;
+
+/**
+ *
+ * @author propa
+ */
+public class Bol102 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Xogo obx = new Xogo();
+        obx.xogador1();}
+    
+}
